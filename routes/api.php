c70fd80b05d7156ea7290eb9bf6dad8c2b38ef81
Mashood Rafi <pro@Mashood-MacBook-Pro-2019.local>
2021-08-31 18:45:29 +0500

    Remove unused route from api.php.

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get("/lunar-delivery-time", "App\Http\Controllers\LunarController@getLunarDeliveryTime")->middleware("validate-time");
